37d1ea4cdacb8e74e2075e3db08e7017463e990b
Josell <vibarjosell@gmail.com>
2023-08-12 14:52:39 +0800

    add code to AvatarController to update and store image in storage_path

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -5,13 +5,18 @@ namespace App\Http\Controllers\Profile;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateAvatarRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AvatarController extends Controller
 {
     public function update(UpdateAvatarRequest $request)
     {
 
-        dd($request->all());
+        $path = $request->file('avatar')->store('avatars');
+
+        auth()->user()->update([
+            'avatar' => storage_path('app') . "/$path"
+        ]);
 
         return redirect(route('profile.edit'))->with('success', 'Uploaded Avatar Successfully!');
     }
